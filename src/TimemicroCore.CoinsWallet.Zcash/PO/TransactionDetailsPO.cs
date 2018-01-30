b388df0b8e2e1d236be095c03cccb40c67e2600a
王珏 <jue.wang@timemicro.tech>
2018-01-30 14:37:25 +0800

    CoinWallet.Zcash

---
+++ src/TimemicroCore.CoinsWallet.Zcash/PO/TransactionDetailsPO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimemicroCore.CoinsWallet.Zcash.PO
+{
+    public class TransactionDetailsPO
+    {
+        public string TxId { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string Category { get; set; }
+    }
+}
---
+++ src/TimemicroCore.CoinsWallet.Zcash/Service/IReceiveNotifyService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimemicroCore.CoinsWallet.Zcash.Service
+{
+    public interface IReceiveNotifyService
+    {
+        void Notify();
+    }
+}
